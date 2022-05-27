7fff7192202b65ef3ee83854ae49fb0850ae5f00
rezaandreannn <andreanreza042@gmail.com>
2022-05-27 21:27:19 +0700

    membuat judul dinamis dengan component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title>
+    <title>{{ $title ?? config('app.name') }} </title>
 
     <!-- Bootstrap -->
     <link href="{{ asset('assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
