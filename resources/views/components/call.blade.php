9f591ce9db4bd2c972bfb0c3eee1c8ec9c7cc915
rezaandreannn <andreanreza042@gmail.com>
2022-06-09 00:17:04 +0700

    menambahkan panggil saya

---
+++ resources/views/components/call.blade.php
@@ -0,0 +1,6 @@
+<a href="https://api.whatsapp.com/send?phone=6289649933542" target="blank"
+    class="badge bg-primary text-decoration-none fst-italic fw-light">call
+    1</a>
+<a href="https://api.whatsapp.com/send?phone=6283160071293" target="blank"
+    class="badge bg-primary text-decoration-none fst-italic fw-light">call
+    2</a>
